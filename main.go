0c698f1ac26deb32216241db24ed0b0257b480e3
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-12-29 11:25:23 +0100

    Jira client proof of concept

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/andygrunwald/go-jira"
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -13,6 +16,32 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	jiraInstance := os.Getenv("JIRA_INSTANCE")
+	jiraUsername := os.Getenv("JIRA_USERNAME")
+	jiraPassword := os.Getenv("JIRA_PASSWORD")
+
+	jiraClient, err := jira.NewClient(nil, jiraInstance)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := jiraClient.Authentication.AcquireSessionCookie(jiraUsername, jiraPassword)
+	if err != nil || res == false {
+		fmt.Printf("Result: %v\n", res)
+		panic(err)
+	}
+
+	projectList, _, _ := jiraClient.Project.GetList()
+
+	for _, project := range *projectList {
+		fmt.Printf("Result: %v\n", project.Name)
+	}
+
 	router := httprouter.New()
 	router.GET("/", index)
 
