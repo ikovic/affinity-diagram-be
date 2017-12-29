3d18d1aab081aa24792bec5895d224e64ed2ae1a
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-12-29 10:38:10 +0100

    Picked a router

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
